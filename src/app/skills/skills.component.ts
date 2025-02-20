2a81571c727ee61f450cfb0345060845dffd30e5
Monish Devineni <monish6468@gmail.com>
2025-02-20 14:36:41 -0500

    updated skillsicons

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,7 +7,7 @@ import { faAws, faGoogle, faMicrosoft,
   faCss3Alt, faReact, faNodeJs,
   faGit, faAngular 
  } from '@fortawesome/free-brands-svg-icons';
-import { faCloud,faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { faCloud,faDatabase,faLeaf } from '@fortawesome/free-solid-svg-icons';
 registerSwiperElements();
 @Component({
   selector: 'app-skills',
@@ -30,14 +30,15 @@ export class SkillsComponent {
   faGit=faGit
   facloud=faCloud
   fadb=faDatabase
+  faLeaf=faLeaf
   faNode=faNodeJs
   techStack = [
-    { name: 'Python', image: 'assets/python.png', delay: '0s' },
-    { name: 'Java', image: 'assets/java.png', delay: '0.2s' },
-    { name: 'JavaScript', image: 'assets/javascript.png', delay: '0.4s' },
-    { name: 'Scala', image: 'assets/scala.png', delay: '0.6s' },
-    { name: 'SQL', image: 'assets/sql.png', delay: '0.8s' },
-    { name: 'MongoDB', image: 'assets/mongodb.png', delay: '1s' },
-    { name: 'Database', image: 'assets/database.png', delay: '1.2s' }
+    { name: 'Python', icon: faPython, delay: '0s' },
+    { name: 'Java', icon: faJava, delay: '0.2s' },
+    { name: 'JavaScript', icon:faJs, delay: '0.4s' },
+    { name: 'SQL', icon: faDatabase, delay: '0.8s' },
+    { name: 'MongoDB', icon: faLeaf, delay: '1s' },
+    { name: 'React', icon: faReact, delay: '1.2s' },
+    { name: 'Angular', icon: faAngular, delay: '1.4s' }
   ];
 }
